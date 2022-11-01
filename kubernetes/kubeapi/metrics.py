c237616191a8434888260a45ca101c4449d2ac94
ehsan <ehsan.shirzadi@gmail.com>
2022-11-01 13:25:48 +0330

    updated: node_metrics calculated

--- kubernetes/kubeapi/metrics.py
+++ kubernetes/kubeapi/metrics.py
@@ -8,7 +8,18 @@ api = client.CustomObjectsApi()
 
 def node_metrics():
     k8s_nodes = api.list_cluster_custom_object("metrics.k8s.io", "v1beta1", "nodes")
+    total_cpu = 0
+    workers_count = 0
     for stats in k8s_nodes['items']:
-        print("Node Name: %s\tCPU: %s\tMemory: %s" % (stats['metadata']['name'], stats['usage']['cpu'], stats['usage']['memory']))
-        print(stats)
+        if 'node-role.kubernetes.io/master' not in stats['metadata']['labels']:
+            workers_count += 1
+            memory = round(int(stats['usage']['memory'][:-2])/1024/1024, 2)
+            cpu = round(int(stats['usage']['cpu'][:-1])/1000000000, 2)
+            total_cpu += cpu
+            print(f"Node Name: {stats['metadata']['name']}\tCPU: {cpu}\tMemory: {memory}GB")
+            print(stats)
+    average_cpu = total_cpu / workers_count
+    print(f'Workers count: {workers_count}, Total CPU: {total_cpu} Average CPU: {average_cpu}')
 
+
+node_metrics()
