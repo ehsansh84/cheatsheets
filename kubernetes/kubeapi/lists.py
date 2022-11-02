c76636898fd9da3b593d9d9a75cd4f0dcf78358a
ehsan <ehsan.shirzadi@gmail.com>
2022-11-02 09:37:40 +0330

    added: list_pod_for_all_namespaces

--- kubernetes/kubeapi/lists.py
+++ kubernetes/kubeapi/lists.py
@@ -5,8 +5,20 @@ configuration = config.load_kube_config(config_file=os.environ.get("KUBECONFIG",
 
 v1 = client.CoreV1Api()
 
-namespaces = [item.metadata.name for item in v1.list_namespace().items]
-print(namespaces)
 
-nodes = [item.metadata.name for item in v1.list_node().items]
-print(nodes)
+def list_namespaces():
+    namespaces = [item.metadata.name for item in v1.list_namespace().items]
+    print(namespaces)
+
+
+def list_nodes():
+    nodes = [item.metadata.name for item in v1.list_node().items]
+    print(nodes)
+
+
+def list_pods():
+    pods = [item.metadata.name for item in v1.list_pod_for_all_namespaces().items]
+    print(pods)
+
+
+list_pods()
